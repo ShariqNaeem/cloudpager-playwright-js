4c47dd8703f148c6e2225952b66656989e4591a9
ShariqNaeem <muhammadshariq.64@gmail.com>
2023-09-21 02:17:36 +0500

    all in one file

--- tests/workpod.spec.js
+++ tests/workpod.spec.js
@@ -25,6 +25,40 @@ test.afterAll(async () => {
     await page.close();
 });
 
+test('Validate that user is able to create the workpod and save it to draft.', async () => {
+    const dashboardPage = new DashboardPage(page)
+    const workpodPage = new WorkpodPage(page)
+
+    await page.waitForTimeout(10000)
+    await dashboardPage.workpodSideNav.click()
+    await page.waitForTimeout(10000)
+    await workpodPage.addWorkpod.click()
+    await workpodPage.setNameAndDescription(workpodData.name, workpodData.description)
+    await workpodPage.addApplicationButton.click()
+    // await workpodPage.table.isVisible()
+    await page.waitForTimeout(10000)
+    await workpodPage.clickOnCheckBox(1)
+    await workpodPage.clickOnCheckBox(2)
+    await workpodPage.saveButton.click()
+
+    await workpodPage.addUserGroupButton.click()
+    // await workpodPage.table.isVisible()
+    await page.waitForTimeout(10000)
+    await workpodPage.clickOnCheckBox(1)
+    await workpodPage.clickOnCheckBox(2)
+
+    await workpodPage.userTab.click()
+    await page.waitForTimeout(10000)
+    await workpodPage.clickOnCheckBox(1)
+    await workpodPage.clickOnCheckBox(2)
+    await workpodPage.saveButton.click()
+    await page.waitForTimeout(10000)
+    await workpodPage.saveDraftButton.click();
+
+    await expect(workpodPage.alertDialog).toContainText('New draft created.')
+    await expect(workpodPage.successMessgae).toContainText('Workpod Created')
+})
+
 test('Go to Draft workpod, Edit it but dont save it, just discard at the end', async () => {
     const dashboardPage = new DashboardPage(page)
     const workpodPage = new WorkpodPage(page)
@@ -61,26 +95,43 @@ test('Go to Draft workpod, Edit it but dont save it, just discard at the end', a
     await expect(workpodPage.alertDialog).toContainText('has been removed from your drafts.')
 })
 
-test('Go to Drafts section and delete any existing Draft.', async () => {
+test('Validate that user is able to edit the workpod and publish it', async () => {
     const dashboardPage = new DashboardPage(page)
-    const workpodPage = new WorkpodPage(page) 
+    const workpodPage = new WorkpodPage(page)
 
     await page.waitForTimeout(10000)
     await dashboardPage.workpodSideNav.click()
     await page.waitForTimeout(10000)
     await workpodPage.draftsSection.click()
     await page.waitForTimeout(10000)
+    await workpodPage.actionButton.click()
+    await workpodPage.editOption.click()
+    await workpodPage.editingAlert.isVisible()
 
-    const workpodName = await workpodPage.firstWorkpodName.textContent()
-    await workpodPage.deleteFirstWorkpod();
-    await expect(workpodPage.alertDialog).toContainText('Workpod deleted.')
-    
+    await workpodPage.setNameAndDescription(workpodData.updatedName, workpodData.updatedDescription)
+    //await page.waitForTimeout(10000)
+    await workpodPage.addButtonInDraft.click({ force: true });
     await page.waitForTimeout(10000)
-    await dashboardPage.workpodSideNav.click()
-    await page.waitForTimeout(10000)
-    await workpodPage.draftsSection.click()
-    await expect(await workpodPage.firstWorkpodName).not.toContainText(workpodName)
+    await workpodPage.clickOnCheckBox(3)
+    await workpodPage.clickOnCheckBox(4)
+    await workpodPage.saveButton.click()
 
+    await page.waitForTimeout(10000)
+    await workpodPage.groupAndUsers.click()
+    await workpodPage.addButtonInDraft.click({ force: true })
+    await page.waitForTimeout(10000)
+    await workpodPage.clickOnCheckBox(3)
+    await workpodPage.clickOnCheckBox(4)
+
+    await workpodPage.userTab.click()
+    await page.waitForTimeout(10000)
+    await workpodPage.clickOnCheckBox(3)
+    await workpodPage.clickOnCheckBox(4)
+    await workpodPage.saveButton.click()
+    await workpodPage.publishButton.click()
+
+    await workpodPage.enterPublishComment('Automated comment...')
+    await expect(workpodPage.alertDialog).toContainText('have been published successfully.')
 })
 
 test('Go to Published Workpod section and Edit any published workpod and then Saved it as a draft', async () => {
@@ -125,4 +176,42 @@ test('Go to Published Workpod section and Edit any published workpod and then Sa
     // await workpodPage.draftsSection.click()
     // await page.waitForTimeout(10000)
     // await expect(workpodPage.firstWorkpodName).toContainText(randomName);
+})
+
+test('Validate that user is able to delete the workpod from the published tab.', async () => {
+    const dashboardPage = new DashboardPage(page)
+    const workpodPage = new WorkpodPage(page)
+
+    await page.waitForTimeout(10000)
+    await dashboardPage.workpodSideNav.click()
+    await page.waitForTimeout(10000)
+    await workpodPage.publishedSection.click()
+    await page.waitForTimeout(10000)
+    await workpodPage.actionButton.click()
+    await workpodPage.deleteOption.click()
+
+    await workpodPage.enterWorkpodNameAndDelete(workpodData.updatedName)
+    await expect(workpodPage.alertDialog).toContainText('Workpod deleted.')
+})
+
+test('Go to Drafts section and delete any existing Draft.', async () => {
+    const dashboardPage = new DashboardPage(page)
+    const workpodPage = new WorkpodPage(page) 
+
+    await page.waitForTimeout(10000)
+    await dashboardPage.workpodSideNav.click()
+    await page.waitForTimeout(10000)
+    await workpodPage.draftsSection.click()
+    await page.waitForTimeout(10000)
+
+    const workpodName = await workpodPage.firstWorkpodName.textContent()
+    await workpodPage.deleteFirstWorkpod();
+    await expect(workpodPage.alertDialog).toContainText('Workpod deleted.')
+    
+    await page.waitForTimeout(10000)
+    await dashboardPage.workpodSideNav.click()
+    await page.waitForTimeout(10000)
+    await workpodPage.draftsSection.click()
+    await expect(await workpodPage.firstWorkpodName).not.toContainText(workpodName)
+
 })
