3b58bb1dbaf88dcb004f032dd8ea1f508d613c06
MuhammadSaad2021 <muhammadshariq.64@gmail.com>
2023-11-02 02:20:33 +0500

    promisfy

---
+++ tests/notepad.spec.js
@@ -0,0 +1,29 @@
+const { exec, spawn } = require("child_process");
+const { test } = require("@playwright/test");
+import { execCommand } from "../utility/command";
+
+test("Launch and Close Notepad++", async ({ page }) => {
+  // console.log("Notepad++ has been launched");
+  const notepadPlusPlusPath = '"C:\\Program Files\\Notepad++\\notepad++.exe"';
+  // await execCommand(notepadPlusPlusPath);
+  await execCommand(notepadPlusPlusPath);
+
+  const kill = 'taskkill /F /IM notepad++.exe';
+  await execCommand(kill);
+
+
+  // const launchNotepadPlusPlus = await exec(
+  //   '"C:\\Program Files\\Notepad++\\notepad++.exe"'
+  // );
+  // console.log("Notepad++ has been launched");
+
+  // await page.waitForTimeout(10000);
+  // console.log("Notepad++ CLOSING");
+
+  // exec(`taskkill /F /IM notepad++.exe`, (error, stdout, stderr) => {
+  //   if (error) {
+  //     console.error(`Error closing Notepad++: ${error.message}`);
+  //   }
+  //   console.log("Notepad++ has been closed");
+  // });
+});
---
+++ utility/command.js
@@ -0,0 +1,14 @@
+const { exec } = require("child_process");
+
+
+export async function execCommand(command) {
+  const result = await new Promise((resolve, reject) => {
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(stdout);
+      }
+    });
+  });
+}
